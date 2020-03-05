41358b1bbcfbcba27dfa4c8d587a07f3f26e1126
EmNudge <emnudge@gmail.com>
2020-03-05 16:13:39 -0500

    added code examples for canvases

--- src/slides/Canvas.svelte
+++ src/slides/Canvas.svelte
@@ -1,9 +1,29 @@
 <script>
   import CanvasCanvas from '../components/CanvasCanvas.svelte';
+  import { onMount } from 'svelte';
 
+  import Highlight from 'svelte-highlight';
+  import { javascript } from 'svelte-highlight/languages';
+  import 'svelte-highlight/styles/dracula.css';
 
+  let code;
+  onMount(async () => {
+    const res = await fetch('/code/jsCanvas.txt');
+    code = await res.text();
+  });
 </script>
+
+<style>
+  .code {
+    overflow: scroll;
+    height: 45%;
+  }
+</style>
 
 <h1>Canvas using 2D Canvas API</h1>
 
 <CanvasCanvas />
+
+<div class="code">
+  <Highlight language={javascript} {code} />
+</div>
--- src/slides/Canvas2.svelte
+++ src/slides/Canvas2.svelte
@@ -1,9 +1,30 @@
 <script>
   import SvgCanvas from '../components/SvgCanvas/SvgCanvas.svelte';
+  import { onMount } from 'svelte';
 
+  import Highlight from 'svelte-highlight';
+  import { javascript } from 'svelte-highlight/languages';
+  import 'svelte-highlight/styles/dracula.css';
 
+  let code;
+  onMount(async () => {
+    const res = await fetch('/code/svgCanvas.txt');
+    code = await res.text();
+  });
+  
 </script>
 
-<h1>Canvas using SVGs</h1>
+<style>
+  .code {
+    overflow: scroll;
+    height: 45%;
+  }
+</style>
+
+<h1>Canvas using 2D Canvas API</h1>
 
 <SvgCanvas />
+
+<div class="code">
+  <Highlight language={javascript} {code} />
+</div>
